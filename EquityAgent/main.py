a40dcff9051d370fb4555a10295947d693f2f24b
Smriti Rajan <smritirajan211@gmail.com>
2025-08-05 12:39:21 -0400

    Wrote script to clean up population data csvs

--- EquityAgent/main.py
+++ EquityAgent/main.py
@@ -1,18 +1,18 @@
 import os
 import sys
-from populationVulnerability import get_place_data
+from populationVulnerability import get_data
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Enter a valid state abbreviation")
     else:
         state = sys.argv[1].upper()
-        df = get_place_data(state)
+        df = get_data(state)
 
         folder = "results"
         os.makedirs(folder, exist_ok=True)
 
-        filename = f"{folder}/{state.lower()}_place_population_{state}.csv"
+        filename = f"{folder}/Population_Vulnerability_{state}.csv"
         df.to_csv(filename, index=False)
 
         print(f"Saved {filename}")
